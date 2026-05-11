10ca22a7f390f2c4cf067cc4bd0153f6e7dd23de
AndreyPascoa <andrey.pascoa@gmail.com>
2026-05-11 09:03:00 -0300

    adicionado o sobre nós

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
+import Carousel from "@/components/carousel/_page";
+import SobreNos from "@/components/home/_page";
 import Navbar from "@/components/navbar/_page";
 
 export default function Home() {
   return (
     <div>
       <Navbar />
+      <Carousel/>
+      <SobreNos />
     </div>
   );
 }
